5774cbca3dd05815138dc245b233ba2ec1cd4c4e
Sergio <Seraml17@gmail.com>
2022-05-05 16:57:51 -0600

    Nuevo Proyecto movie

---
+++ movie-react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function App() {
+
+  const {Header, Content} = Layout;
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+}
+
+export default App;
